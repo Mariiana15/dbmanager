f5b5694411942ec5378a78506aad52dc3b31468c
Jose <mariana.lemus.g15@gmail.com>
2022-06-21 20:46:51 +0200

    server MySQL2

--- dbmanager.go
+++ dbmanager.go
@@ -10,7 +10,7 @@ import (
 )
 
 func newConnect() *sql.DB {
-	db, err := sql.Open("mysql", "tfm:tfm@tcp(35.224.168.227:3306)/hqa")
+	db, err := sql.Open("mysql", "dbhqa1:dbhqa1@tcp(35.224.168.227:3306)/hqa")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
