269bf540f782088fddc37e4802e67b87b7ca0648
Jose <jlparra@bhub.com>
2023-03-11 16:11:11 -0500

    update format Task

--- types.go
+++ types.go
@@ -49,42 +49,42 @@ type CustomField struct {
 }
 
 type Task struct {
-	Id            	string        `json:"id"`
-	Hid           	string        `json:"hid"`
-	Gid           	string        `json:"gid"`
-	UserId        	string        `json:"userId"`
-	Name         	string        `json:"name"`
-	Notes         	string        `json:"notes"`
-	CustomField   	[]CustomField `json:"custom_fields"`
-	Link          	string        `json:"permalink_url"`
-	Story         	[]Story       `json:"stories"`
-	Dependecies   	[]General     `json:"dependencies"`
-	State         	string        `json:"state"`
-	TypeTest      	string        `json:"typeTest"`
-	TypeTestId    	string        `json:"typeTestId"`
-	TypeUS        	string        `json:"typeUS"`
-	UserStory     	string        `json:"userStory"`
-	Priority      	int           `json:"priority"`
-	Alerts        	int           `json:"alerts"`
-	Scripts       	int           `json:"scripts"`
-	Date          	int64         `json:"date"`
-	UrlAlert      	string        `json:"urlAlert"`
-	UrlScript     	string        `json:"urlScript"`
-	AddInfo       	int8          `json:"addInfo"`
-	Test          	General       `json:"test"`
-	Result        	Result        `json:"result"`
-	Tecnologies   	string        `json:"technologies"`
-	Requirement   	string        `json:"requirement"`
-	Architecture  	string        `json:"architecture"`
-	Industry      	string        `json:"industy"`
-	RiskIndustry  	string        `json:"riskIndustry"`
+	Id              string        `json:"id"`
+	Hid             string        `json:"hid"`
+	Gid             string        `json:"gid"`
+	UserId          string        `json:"userId"`
+	Name            string        `json:"name"`
+	Notes           string        `json:"notes"`
+	CustomField     []CustomField `json:"custom_fields"`
+	Link            string        `json:"permalink_url"`
+	Story           []Story       `json:"stories"`
+	Dependecies     []General     `json:"dependencies"`
+	State           string        `json:"state"`
+	TypeTest        string        `json:"typeTest"`
+	TypeTestId      string        `json:"typeTestId"`
+	TypeUS          string        `json:"typeUS"`
+	UserStory       string        `json:"userStory"`
+	Priority        int           `json:"priority"`
+	Alerts          int           `json:"alerts"`
+	Scripts         int           `json:"scripts"`
+	Date            int64         `json:"date"`
+	UrlAlert        string        `json:"urlAlert"`
+	UrlScript       string        `json:"urlScript"`
+	AddInfo         int8          `json:"addInfo"`
+	Test            General       `json:"test"`
+	Result          Result        `json:"result"`
+	Tecnologies     string        `json:"technologies"`
+	Requirement     string        `json:"requirement"`
+	Architecture    string        `json:"architecture"`
+	Industry        string        `json:"industry"`
+	RiskIndustry    string        `json:"riskIndustry"`
 	TrendIndustry   string        `json:"trendIndustry"`
 	DetailIndustry  string        `json:"detailIndustry"`
 	Stakeholder     string        `json:"stakeHolder"`
 	RiskStakeholder string        `json:"riskStakeholder"`
-	Activity 		string        `json:"activity"`
-	RiskActivity	string        `json:"riskActivity"`
-	Risk			string        `json:"risk"`
+	Activity 	    string        `json:"activity"`
+	RiskActivity    string        `json:"riskActivity"`
+	Risk		    string        `json:"risk"`
 }
 
 type Result struct {
